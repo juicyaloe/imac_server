c8db7f99251a24bab2b90963b7ab75aae295a5df
Min-jae Kim <mink4661@snu.ac.kr>
2022-10-20 02:32:25 +0900

    CORS 처리

--- backend/django_backend/settings.py
+++ backend/django_backend/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'temp_data',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -53,6 +56,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
+                         'http://localhost:3000']
 
 ROOT_URLCONF = "django_backend.urls"
 
